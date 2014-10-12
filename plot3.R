b20735d38253f2d68bf7773e33d8b0680d1ae658
Cel Kulasekaran <cel@kulasekaran.com>
2014-10-12 15:42:40 -0400

    Added plot3.R code.

---
+++ plot3.R
@@ -0,0 +1,31 @@
+# Load required packages for processing data
+require(data.table)
+require(dplyr)
+
+# load data set
+DT<-fread(input="household_power_consumption.txt",stringsAsFactors=FALSE,na.strings=c("?"," ? "))
+
+
+# Get data from the two days from the assignment description
+Data<-DT[(DT$Date == "1/2/2007" | DT$Date == "2/2/2007"),]
+
+# reorder by date, then by time
+Data<- Data[order(Date,Time)]
+
+# construct a date time vector
+DTime <- strptime(paste(Data$Date, Data$Time, sep=","), format="%d/%m/%Y,%H:%M:%S")
+
+# create a line plot
+plot(DTime,Data$Sub_metering_1, type="lines", col="black", xlab="", ylab="Energy sub metering")
+lines(DTime,Data$Sub_metering_2,col="red")
+lines(DTime,Data$Sub_metering_3,col="blue")
+
+# Show legend top right
+legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+       col=c("black","red","blue"), lwd=1, cex=0.8)
+
+# Copy my plot to a PNG file
+dev.copy(png, file = "plot3.png")
+
+# Don't forget to close the PNG device!
+dev.off()
